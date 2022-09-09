7f5208137b3f678989bc20288c72bc207fcf76c6
RekiDunois <rekidunois+github@gmail.com>
2022-09-09 14:07:41 +0800

    main: add conditions to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import { useState } from 'react'
 import './App.css'
-import { Deck } from './components/deck'
-import { DeckImporter } from './components/deck-importer'
+import { Deck } from './panels/deck'
+import { Conditions } from './panels/conditions'
+import { Grid, GridItem, chakra, Box } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <Deck
-        data={{
-          id: 1,
-          name: '114514',
-          quantity: 40,
-          main: [],
-          side: [],
-          ex: [],
-        }}
-      />
-    </div>
+    <Box textAlign={'center'} width="100vw" height="100vh" padding={'2em'}>
+      <chakra.main width={'full'}>
+        <Grid
+          templateRows="repeat(9, 10vh)"
+          templateColumns="repeat(9, 10fr)"
+          column={2}
+        >
+          <GridItem rowSpan={9} colSpan={2}>
+            <Deck
+              data={{
+                id: 1,
+                name: '114514',
+                quantity: 40,
+                main: [],
+                side: [],
+                ex: [],
+              }}
+            />
+          </GridItem>
+          <GridItem colSpan={7} rowSpan={8}>
+            <Conditions />
+          </GridItem>
+        </Grid>
+      </chakra.main>
+    </Box>
   )
 }
 
